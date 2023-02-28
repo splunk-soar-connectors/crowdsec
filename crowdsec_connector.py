7e484c85b24f194722959af4876ccd1261fcd22c
Ishan Shah <ishans@splunk.com>
2023-02-28 12:50:47 +0530

    Added sys module

--- crowdsec_connector.py
+++ crowdsec_connector.py
@@ -309,6 +309,7 @@ class CrowdsecConnector(BaseConnector):
 
 def main():
     import argparse
+    import sys
 
     argparser = argparse.ArgumentParser()
 
@@ -352,7 +353,7 @@ def main():
             session_id = r2.cookies["sessionid"]
         except Exception as e:
             print("Unable to get session id from the platform. Error: " + str(e))
-            exit(1)
+            sys.exit(1)
 
     with open(args.input_test_json) as f:
         in_json = f.read()
@@ -369,7 +370,7 @@ def main():
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
 
-    exit(0)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
