4fe7ae5cbbb66d6e3162ec8bcedf06951e71b8d4
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2023-03-06 15:57:39 +0530

    CrowdSec: New App - Initial Implementation (#1)
    * :tada: Init
    
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>
    
    * Update readme.html
    
    * Update and rename readme.html to README.md
    
    * Fill up the doc
    
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>
    
    * Fill up the doc
    
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>
    
    * Test connectivity action impl
    
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>
    
    * Set user agent  to crowdsec-splunk-soar/v1.0.0
    
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>
    
    * Add readme.html
    
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>
    
    * Fix linter
    
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>
    
    * Updated code according to dev checks
    
    * Pre-commit changes
    
    * Added release notes
    
    * Added sys module
    
    ---------
    
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>
    Co-authored-by: Ishan Shah <ishans@splunk.com>

---
+++ crowdsec_consts.py
@@ -0,0 +1,18 @@
+# File: crowdsec_consts.py
+#
+# Copyright (c) 2023 CrowdSec
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
+CROWDSEC_BASE_URL = "https://cti.api.crowdsec.net/v2"
+CROWDSEC_USER_AGENT = "crowdsec-splunk-soar/v1.0.0"
+DEFAULT_TIMEOUT = 30
